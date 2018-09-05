756ddc35f1eeca7d8932260b7dc10495986a2e8f
miknhoj <kimjohan86@gmail.com>
2018-09-05 09:16:28 -0400

    add pseudo code for functions to start building

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
 $(() => {
 console.log("im in here")
 
-var totalScore = 0
+
+// Need variable to keep track of score - use jQuery to manipulate score div?
+
+updateScore = function () {
+    var totalScore = 0 
+console.log(totalScore)
+}
+
+// Need function that will update score based on whether user answered correctly or not and add or subtract to total score. 
+// Probably an if statement !== / ===
+
+// Need function that will remove box from the board when a question is answered or change the color. 
+// Probably jQuery click event that changes the color
+
+// 
 
 $('')
 
