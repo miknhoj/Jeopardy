dafba786154fb18289a88b7babf247ea49672652
miknhoj <kimjohan86@gmail.com>
2018-09-05 14:41:54 -0400

    added jQuery functions that adds or subtracts points that can only be seen in the console

--- main.js
+++ main.js
@@ -3,22 +3,35 @@ const player = {
     score: 0
 }
 
-// Need function that will update score based on whether user answered correctly or not and add or subtract to total score. 
-// Need to add a class for the correct answer in html to target
-// Probably an if statement !== / ===
-// Use data-tag
+const ScoreDisplay = {
+
+    scoreUpdater: function (score) {
+        $('#score')
+        if ()
+    }
+}
+
 
 $(() => {
 console.log("im in here")
 
+// Need function that will update score based on whether user answered correctly 
+// or not and add or subtract to total score. 
+// Need to add a class for the correct answer in html to target
+// Probably an if statement !== / ===
+// Use data-tag
 
+$('.correct').click(function(event){
+    alert("Correct")
+    let points = $(event.currentTarget).data('score')
+    player.score += points
+})
 
-
-
-
-
-
-
+$('.incorrect').click(function(event){
+    alert("Sorry, incorrect")
+    let points = $(event.currentTarget).data('score')
+    player.score -= points
+})
 
 // Need function that will remove box from the board when a question is answered or change the color. 
 // Probably jQuery click event that changes the color
