43c3c45fcba234c7844a9e6c7b671b4edba2c0bf
miknhoj <kimjohan86@gmail.com>
2018-09-05 11:31:00 -0400

    added data-tag to test question

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
+const player = {
+    name: 'playerone',
+    score: 0
+}
+
+// Need function that will update score based on whether user answered correctly or not and add or subtract to total score. 
+// Need to add a class for the correct answer in html to target
+// Probably an if statement !== / ===
+// Use data-tag
+
 $(() => {
 console.log("im in here")
 
 
-// Need variable to keep track of score - use jQuery to manipulate score div?
 
-updateScore = function () {
-    var totalScore = 0 
-console.log(totalScore)
-}
 
-// Need function that will update score based on whether user answered correctly or not and add or subtract to total score. 
-// Probably an if statement !== / ===
+
+
+
+
+
 
 // Need function that will remove box from the board when a question is answered or change the color. 
 // Probably jQuery click event that changes the color
 
 // 
 
-$('')
+// $('')
 
 })
