491593dfdbd2c811b78f473abdc6caddd735cfe3
miknhoj <kimjohan86@gmail.com>
2018-09-07 10:15:48 -0400

    updated reademe

--- main.js
+++ main.js
@@ -1,97 +1,88 @@
-console.log('hello')
+console.log("hello");
 
 // Player object
 const player = {
-    name: 'playerone',
-    score: 0
-}
+  name: "playerone",
+  score: 0
+};
 
 // Updating score on html
 function updateTotalScore() {
-    $('#score').text(player.score)
+  $("#score").text(player.score);
 }
 
 // Updating ranking on html
 function rankUpdater(string) {
-$('#ranking').text(string)
+  $("#ranking").text(string);
 }
 
-function rank(){
-    let rank
-    let tscore = player.score
-        if (tscore <= 0) {
-            rank = 'Beginner'
-        } else if (tscore > 0 && tscore < 120) {
-            rank = 'Novice'
-        } else if (tscore > 120 && tscore < 360) {
-            rank = 'Intermediate'
-        } else if (tscore > 360 && tscore < 540) {
-            rank = 'Expert'
-        } else {
-            rank = 'Master'
-        }
-    rankUpdater(rank)
+function rank() {
+  let rank;
+  let tscore = player.score;
+  if (tscore <= 0) {
+    rank = "Beginner";
+  } else if (tscore > 0 && tscore < 120) {
+    rank = "Novice";
+  } else if (tscore > 120 && tscore < 360) {
+    rank = "Intermediate";
+  } else if (tscore > 360 && tscore < 540) {
+    rank = "Expert";
+  } else {
+    rank = "Master";
+  }
+  rankUpdater(rank);
 }
-
-
 
 //create click event on the gameboard
 // get the specific target of the click
 // disable button
 
-$('.btn').on('click', function(){
-    $(event.currentTarget).addClass('disabled', true)
-})
-
-
-
+$(".btn").on("click", function() {
+  $(event.currentTarget).addClass("disabled", true);
+});
 
 $(() => {
-console.log("im in here")
+  console.log("im in here");
 
-// Need function that will update score based on whether user answered correctly 
-// or not and add or subtract to total score. 
-// Need to add a class for the correct answer in html to target
-// Probably an if statement !== / ===
-// Use data-tag
+  // Need function that will update score based on whether user answered correctly
+  // or not and add or subtract to total score.
+  // Need to add a class for the correct answer in html to target
+  // Probably an if statement !== / ===
+  // Use data-tag
 
-// $('.btn').one('click',function (){
-//     var dataTarget = $(this).attr('data-target')
-//     console.log(dataTarget)
-//     setTimeout(function() {
-//         console.log("INSIDE SETTIMEOUT")
-//         $().attr('disabled', 'disabled')}, 2000)
-// })
+  // $('.btn').one('click',function (){
+  //     var dataTarget = $(this).attr('data-target')
+  //     console.log(dataTarget)
+  //     setTimeout(function() {
+  //         console.log("INSIDE SETTIMEOUT")
+  //         $().attr('disabled', 'disabled')}, 2000)
+  // })
 
-// $('.btn').on('click', function() {
-//     // alert('hey this happens first')
-//     $(this).attr('disabled', 'true')
-// })
+  // $('.btn').on('click', function() {
+  //     // alert('hey this happens first')
+  //     $(this).attr('disabled', 'true')
+  // })
 
+  $(".correct").click(function(event) {
+    alert("Correct");
+    let points = $(event.currentTarget).data("score");
+    player.score += points;
+    updateTotalScore();
+    rank();
+  });
 
-$('.correct').click(function(event){
-    alert("Correct")
-    let points = $(event.currentTarget).data('score')
-    player.score += points
-    updateTotalScore()
-    rank()
-})
+  $(".incorrect").click(function(event) {
+    alert("Sorry, incorrect");
+    let points = $(event.currentTarget).data("score");
+    player.score -= points;
+    updateTotalScore();
+    rank();
+  });
 
+  // Need function that will remove box from the board when a question is answered or change the color.
+  // Probably jQuery click event that changes the color
 
-$('.incorrect').click(function(event){
-    alert("Sorry, incorrect")
-    let points = $(event.currentTarget).data('score')
-    player.score -= points
-    updateTotalScore()
-    rank()
-})
+  //
 
-
-// Need function that will remove box from the board when a question is answered or change the color. 
-// Probably jQuery click event that changes the color
-
-// 
-
-// $('')
-
-})
+  // $('')
+});
---
+++ readme.md
@@ -0,0 +1,13 @@
+For WDI17 Unit 1 project I decided to create the game, 'Jeopardy'. The theme that I implemented was Pokemon. I used the Bootstrap to utilize their modal function so that a modal with the question and answer would appear. If a correct choice is clicked the player's total score at the bottom of the screen increases. If they click the incorrect answer, the total score decreases by the amount of the value of the question they chose. The win condition is a ranking system. Based on your total score - the ranking changes and the goal is to obtain Pokemon Master rank.
+
+Link to Netify
+
+
+Link to Trello
+https://trello.com/b/H8lDdM32/jeopardy
+
+Picture of wireframes in github
+
+Technologies used: Bootstrap, jQuery
+
+Version 2
